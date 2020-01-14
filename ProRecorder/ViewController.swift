10f26e5b2582a67aae0483f233b771c7d6b8f1ea
NeoKolian <pochekuevn@gmail.com>
2020-01-14 17:54:30 +0400

    commit for code compare

--- ProRecorder/ViewController.swift
+++ ProRecorder/ViewController.swift
@@ -7,21 +7,87 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var playButton: UIButton!
     
+    var soundRecorder : AVAudioRecorder!
+    var soundPlayer: AVAudioPlayer!
+    
+    var fileName: String = "audioFile.m4a"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupRecorder()
+        playButton.isEnabled = false
     }
 
-    @IBAction func recordAction(_ sender: UIButton) {
-    }
-    @IBAction func playAction(_ sender: UIButton) {
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        
+        return paths[0]
     }
     
+    func setupRecorder() {
+        let audioFileName = getDocumentsDirectory().appendingPathComponent(fileName)
+        let recordSetting = [AVFormatIDKey : kAudioFormatAppleLossless, AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue, AVEncoderBitRateKey : 320000, AVNumberOfChannelsKey : 2, AVSampleRateKey : 44100.2] as [String : Any]
+    
+        do {
+            soundRecorder = try AVAudioRecorder(url: audioFileName, settings: recordSetting)
+            soundRecorder.delegate = self
+            soundRecorder.prepareToRecord()
+        } catch {
+            print (error)
+        }
+    }
+    
+    func setupPlayer() {
+        let audioFileName = getDocumentsDirectory().appendingPathComponent(fileName)
+        do {
+            soundPlayer = try AVAudioPlayer(contentsOf: audioFileName)
+            soundPlayer.delegate = self
+            soundPlayer.prepareToPlay()
+            soundPlayer.volume = 1.0
+        } catch {
+            print(error)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        playButton.isEnabled = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        recordButton.isEnabled = true
+        playButton.setTitle("Play", for: .normal)
+    }
+    
+    @IBAction func recordAction(_ sender: UIButton) {
+        if recordButton.titleLabel?.text == "Record" {
+            soundRecorder.record()
+            recordButton.setTitle("Stop", for: .normal)
+            playButton.isEnabled = false
+        } else {
+            soundRecorder.stop()
+            recordButton.setTitle("Record", for: .normal)
+            playButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func playAction(_ sender: UIButton) {
+        if playButton.titleLabel?.text == "Play" {
+            playButton.setTitle("Stop", for: .normal)
+            recordButton.isEnabled = false
+            setupPlayer()
+            soundPlayer.play()
+        } else {
+            soundPlayer.stop()
+            playButton.setTitle("Play", for: .normal)
+            recordButton.isEnabled = false
+        }
+    }
 }
 
